5680682b09b179f34710a5771f9d12edcc3f770f
jaloma22 <jaloma22@protonmail.com>
2023-06-03 21:03:12 +0000

    Third practice round.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,3 +1,8 @@
+
+# This program is designed to make simple mathematical operations,
+# either through an interactive prompt, or by options used during execution.
+
+
 #include <iostream>
 
 main() {
